a98666fc9ea05ec7314b0118170af56c58f648ac
BogdanKozlov35 <bkozlov35@gmail.com>
2024-03-13 15:06:02 +0200

    Initial commit

---
+++ HW_task_04_01.py
@@ -0,0 +1,29 @@
+#fh = open('salary.txt', 'w')
+#fh.write("Alex Korp,3000\nNikita Borisenko,2000\nSitarama Raju,1000")
+#fh.close()
+
+#fh = open('salary.txt', 'r')
+#all_file = fh.read()
+#print(all_file)
+#fh.close()
+
+import pathlib
+import sys
+def total_salary_2(path):
+    # відкриваємо для читання відомість зп із файла test.txt
+    try:
+        with open("salary.txt", "r", encoding="utf-8") as fh:
+            total = 0
+            count = 0
+            for line in fh:
+                lines = line.strip().split(',')
+                salary = int(lines[1])
+                total += salary
+                count += 1
+            average = total / count
+            return total, average
+    except FileNotFoundError:
+        return "Не вдалося знайти файл"
+
+total, average = total_salary_2("/goit-algo-hw-04/HW_task_04_01.py")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
